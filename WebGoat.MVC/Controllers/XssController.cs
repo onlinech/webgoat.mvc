8c1c17668fed3eed27cfb0a6e9ed9bbaf55951bf
sie <stefan.sieber@online.ch>
2016-06-01 14:19:54 +0200

    Configured Razor template support. Added Xss Example.

---
+++ WebGoat.MVC/Controllers/XssController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace WebGoat.MVC.Controllers
+{
+    public class XssController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to the Xss Example";
+
+            return View();
+        }
+
+    }
+}
