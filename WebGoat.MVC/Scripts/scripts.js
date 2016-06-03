16986ba1ba97c0032b59d1df4c4eaab43340b0b8
sie <stefan.sieber@online.ch>
2016-06-03 15:52:33 +0200

    Xss example 1 (missing jsencode)

---
+++ WebGoat.MVC/Scripts/scripts.js
@@ -0,0 +1,29 @@
+﻿// taken from http://jsfiddle.net/BaylorRae/vwvAd/
+(function ($) {
+    $.fn.flashMessage = function (options) {
+
+        options = $.extend({
+            text: 'Done',
+            time: 10000,
+            how: 'before',
+            class_name: ''
+        }, options);
+
+        return $(this).each(function () {
+            if ($(this).parent().find('.flashMessage').get(0))
+                return;
+
+            var message = $('<div />', {
+                'class': 'flashMessage ' + options.class_name,
+                text: options.text
+            }).hide().fadeIn('fast');
+
+            $(this).html(message);
+
+            message.delay(options.time).fadeOut('normal', function () {
+                $(this).remove();
+            });
+
+        });
+    };
+})(jQuery);
