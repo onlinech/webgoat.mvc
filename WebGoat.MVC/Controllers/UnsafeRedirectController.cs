3d80919139044f6099cec78d1fc0d56ad605ebde
sie <stefan.sieber@online.ch>
2016-09-06 14:34:12 +0200

    added unsafe redirect example

---
+++ WebGoat.MVC/Controllers/UnsafeRedirectController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGoat.MVC.Controllers
+{
+    public class UnsafeRedirectController : Controller
+    {
+        // GET: UnsafeRedirect
+        public ActionResult Login(string redirectUrl)
+        {
+	        if (string.IsNullOrEmpty(redirectUrl))
+	        {
+		        redirectUrl = "/";
+	        }
+	        return View(model: redirectUrl);
+        }
+
+	    [HttpPost]
+	    public ActionResult Login(string username, string password, string redirectUrl)
+	    {
+		    // login would happen here...
+
+			return Redirect(redirectUrl);
+	    }
+    }
+}
