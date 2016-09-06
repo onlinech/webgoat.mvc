5005ed6294ec50423c006dac022e1e8077ca45be
sie <stefan.sieber@online.ch>
2016-09-06 11:15:15 +0200

    Direct reference example & update to OWASP 2013.

---
+++ WebGoat.MVC/Controllers/DirectRefController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGoat.MVC.Controllers
+{
+    public class DirectRefController : Controller
+    {
+	    public ActionResult Index()
+	    {
+		    return View();
+	    }
+
+        public ActionResult Files(string fileName)
+        {
+			string path = HttpContext.Server.MapPath("~/Content/files/" + fileName);
+	        return new ContentResult {ContentType = "text", Content = System.IO.File.ReadAllText(path)};
+        }
+    }
+}
